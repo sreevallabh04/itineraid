9c82f80af4bf7e9b304ae5b282744a975c5e7ca3
sreevallabh <srivallabhkakarala@gmail.com>
2025-05-08 11:58:38 +0530

    Final cleanup

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -60,13 +60,15 @@ const config = {
       },
       keyframes: {
         "accordion-down": {
-          from: { height: 0 },
+          from: { height: "0px" }, // <-- fix
           to: { height: "var(--radix-accordion-content-height)" },
         },
         "accordion-up": {
           from: { height: "var(--radix-accordion-content-height)" },
-          to: { height: 0 },
+          to: { height: "0px" }, // <-- fix
         },
+      
+      
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
